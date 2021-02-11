9152a914389e06a8e926d22f7441e499bb225c44
Eyup Okur <eyupokur7@gmail.com>
2021-02-11 13:11:01 +0300

    first commit

---
+++ http-url-file/app.js
@@ -0,0 +1,18 @@
+const http = require("http");
+const url = require("url");
+const fs = require("fs");
+
+http.createServer(function(req,res){
+    res.writeHead(200,{"Content-Type": "text/html"});
+    let returnErr = "<h1>Oops! 404 Page Not Found</h1>"
+    let q  = url.parse(req.url,true); // true olmasa ne olur 
+    let fn = "./" + q.pathname + ".html";
+    fs.readFile(fn,function(err,data){
+        if(err){
+            return res.end(returnErr);
+        }
+        res.write(data);        
+        return res.end();
+        
+    })
+}).listen(8080);
